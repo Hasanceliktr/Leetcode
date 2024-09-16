1735b69867b9a5af2bd300027cd6f460c9fab067
Hasan Çelik <hasancelikdeveloper@gmail.com>
2024-09-16 22:27:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        FindMinimum findMinimum = new FindMinimum();
+        int dizi [] = {3,6,9,3,7};
+        int sonuc = findMinimum.minimumOperations(dizi);
+        System.out.println(sonuc);
+
+    }
+
+}
